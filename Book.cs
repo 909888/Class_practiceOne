e1c5ac406b450ef3325977a05ef1b42202e156e3
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-10-18 18:42:43 -0700

    upload

---
+++ Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Practice
+{
+    class Book
+    {   //class is just a blueprint for a program
+        public string title;
+        public string author;
+        public int pages; 
+        
+
+               //constructor: a special method
+        //you can use this to pass directly to a parameter
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+
+
+        //constructor. a different way of using em:
+        public Book() { }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //how to create an object:
+            //using the constructor the pass in parameters
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+
+
+            //book1.title = "Harry Potter";
+            //book1.author = "JK Rowling";
+            //book1.pages = 400;
+
+
+            Book book2 = new Book("Lord of the Rings", "Tolkein", 700);
+
+            //use this to change information
+            //title would change from the lord of the rings to the hobbit
+            book2.title = "The Hobbit";
+
+            //book2.title = "Lord of the Rings";
+            //book2.author = "Tolkein";
+            //book2.pages = 700;
+
+
+            Book book3 = new Book();
+
+            
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
